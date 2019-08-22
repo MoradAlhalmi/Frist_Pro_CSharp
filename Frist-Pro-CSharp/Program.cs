d23ad6658a3e11de6dcd282dbce9988fcf607c0a
Morad <Morad@DESKTOP-5EFJFEO>
2019-08-22 15:11:41 +0300

    Add project files.

---
+++ Frist-Pro-CSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frist_Pro_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //my frist program
+            Console.WriteLine("hello word");
+            Console.ReadKey();
+        }
+    }
+}
